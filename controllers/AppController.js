53806ac22f7329850bddac42bda16252d65bd04a
Christian Koranteng <korantengchristian@gmail.com>
2023-05-29 13:52:44 +0000

    fix

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,20 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-const AppController = {
-  getStatus(req, res) {
+class AppController {
+  static getStatus(req, res) {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
 
     res.status(200).json({ redis: redisStatus, db: dbStatus });
-  },
+  }
 
-  getStats(req, res) {
-    const usersCount = dbClient.nbUsers();
-    const filesCount = dbClient.nbFiles();
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
 
     res.status(200).json({ users: usersCount, files: filesCount });
-  },
-};
+  }
+}
 
 module.exports = AppController;
